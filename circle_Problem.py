cb1752fc197526f7e68993284bd65ae471e69da2
gabdai <96576127+gabdai@users.noreply.github.com>
2021-12-27 23:12:47 +0530

    Add files via upload

---
+++ circle_Problem.py
@@ -0,0 +1,5 @@
+#take radius as input and calculate area as output
+radius = float(input("please enter the radius"))
+print(radius)
+area = 3.14*radius**2
+print(" area of the circle with",radius,"is",float(area))
